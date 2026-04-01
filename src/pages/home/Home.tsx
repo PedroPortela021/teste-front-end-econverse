cb4d0f6e4e6efbf0180eb6f2448b7d85ec90cd12
Pedrowill21 <pedrowilliam2999@gmail.com>
2026-04-01 12:33:16 -0300

    refactor: update NavMenu and TopBar tests for consistent naming and improve Home component layout with new sections

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,28 @@
+import { NavMenu } from '../../components/layout/Header/NavMenu/NavMenu'
+import { Navbar } from '../../components/layout/Header/Navbar/Navbar'
+import { TopBar } from '../../components/layout/Header/TopBar/TopBar'
 import styles from './Home.module.scss'
 
 export function Home() {
-  return <main className={styles.home}></main>
+  return (
+    <main className={styles.home}>
+      <header>
+        <TopBar />
+        <Navbar />
+        <NavMenu />
+      </header>
+
+      <section className={styles.section}>
+        <div className={styles.container}>Conteudo da secao 1</div>
+      </section>
+
+      <section className={styles.fullWidthSection}>
+        <div className={styles.container}>Conteudo da secao full width</div>
+      </section>
+
+      <section className={styles.section}>
+        <div className={styles.container}>Conteudo da secao 2</div>
+      </section>
+    </main>
+  )
 }
